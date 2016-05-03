9f217eaf359a98ccd3c418b554fbe005a6ddf13f
Michael C. Martinez <michaelmartinez2017@u.northwestern.edu>
2016-05-02 20:57:06 -0500

    Finalized the structure of the JSON payload

--- app.py
+++ app.py
@@ -11,14 +11,11 @@ def index():
 @app.route('/search', methods=['GET'])
 def search():
     search = request.args.get('searchQuery')
-    # query = urllib.quote(search)
-
     results = wikipedia.search('"' + search + '"', results=5, suggestion= 0)
     resArray = []
-    d = {}
 
     for x in range(0,4):
-
+        d = {}
         title = '"' + results[x] + '"'
         article = wikipedia.page(title)
         content = article.content
@@ -28,51 +25,26 @@ def search():
         # image = article.images[1]
         text = content.split("==")
 
-        headers = []
-        # print text
-        # for n in range(0,len(text) - 1):
-        #     if (n % 2 == 0):
-        #         # i = 0
-        #         # headers[i] = text[n]
-        #         # i = i + 1
-        #         print text[n]
-        #         d["content"] = text[n]
-        #     #     d["headers"] = text[n]
-        #     # else:
-        #     #     d["content"] = text[n]
-        #
-        # # d["image"] = image
-
-        d["summary"] = summary
-        d["title"] = title
-        resArray.append(d)
-        return resArray[x]
+        sections = []
+        s = {}
+        for n in range(0,len(text)):
+            if (n % 2 != 0):
+                s["header"] = text[n]
+                print text[n]
+            else:
+                s["content"] = text[n]
+                print text[n]
+                sections.append(s)
+                s = {}
 
         # d["image"] = image
+        d["sections"] = sections
         d["summary"] = summary
         d["title"] = title
         resArray.append(d)
-        # print content
-        # print text
-        print title
-        for n in range(0,len(text) - 1):
-            if (n % 2 != 0):
-                print text[n]
-
-
-        print "******************************************"
-
-    # class wikipedia.WikipediaPage(title= )
-
-    # endPoint = 'https://en.wikipedia.org/w/api.php?action=query&titles=' + query + '&redirects&prop=revisions&rvprop=content&format=json&formatversion=2&continue='
-    # r = requests.get(endPoint)
-    # result = jsonify(r.json())
-    # for x in secondQuery:
-    # print content
 
     # print resArray
-    # return jsonify(resArray)
-    return "done!"
+    return jsonify(articles = resArray)
 
 if __name__ == "__main__":
     app.run(debug = True)
