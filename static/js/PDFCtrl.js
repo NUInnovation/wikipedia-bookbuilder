15a058b3806c0a8c90329a1d8e77c125eccc565c
Junjie Li <jj.muyang@gmail.com>
2016-05-16 19:10:36 -0500

    fully functional now

--- static/js/PDFCtrl.js
+++ static/js/PDFCtrl.js
@@ -28,7 +28,7 @@ $( "#searchButton" ).click(function() {
                    var ee = $('<div class="list-group">'+
                      '<a href="#design" class="list-group-item" >'+
                        '<h4>' + sec[k].header + '</h4></a>' +
-                       '<input id="section-' + k + '" type="checkbox" name="checkbox" checked="checked" value="' + sec[k].header +  " " + sec[k].content + '">' +
+                       '<input id="section-' + k + '" type="checkbox" name="checkbox" value="' + sec[k].header +  " " + sec[k].content + '">' +
                       //  '<div class="list-group-item">' + sec[k].content+ '</div>' +
                    '</div>');
 
@@ -50,63 +50,52 @@ $( "#searchButton" ).click(function() {
              var $elem = $('.section-'+num);
              var id = $(this).attr('id');
              var value = $('#' + id + '').val();
-             $('#book').append('<h3>' + value + '</h3>');
+             $('#book').append('<div>' + value + '</div>');
              console.log($('#' + id + '').val());
 
-            //  var doc = new jsPDF();
-             //
-            //  // We'll make our own renderer to skip this editor
-            //  var specialElementHandlers = {
-            //    '#editor': function(element, renderer){
-            //      return true;
-            //    }
-            //  };
-             //
-            //  doc.fromHTML($('#book').get(0), 15, 15, {
-            //    'width': 170,
-            //    // 'margin': 1,
-            //   //  'pagesplit': true,
-            //    'elementHandlers': specialElementHandlers
-            //  });
-            //  //doc.save('Test.pdf');
-             //
-            //  $('#open').click(function(){
-            //    // doc.setFont("calibri");
-            //    doc.output('dataurlnewwindow');
-            //  });
-            //  $('#download').click(function(){
-            //    // doc.setFont("calibri");
-            //    doc.save('test.pdf');
-            //  });
-
-             //doc function ends here
-
            });
-           var doc = new jsPDF();
-
-           // We'll make our own renderer to skip this editor
-           var specialElementHandlers = {
-             '#editor': function(element, renderer){
-               return true;
-             }
-           };
-
-           doc.fromHTML($('#articles').get(0), 15, 15, {
-             'width': 170,
-             // 'margin': 1,
-            //  'pagesplit': true,
-             'elementHandlers': specialElementHandlers
-           });
-           //doc.save('Test.pdf');
 
            $('#open').click(function(){
+             var doc = new jsPDF();
+             // We'll make our own renderer to skip this editor
+             var specialElementHandlers = {
+               '#editor': function(element, renderer){
+                 return true;
+               }
+             };
+
+             doc.fromHTML($('#book').get(0), 15, 15, {
+               'width': 170,
+               // 'margin': 1,
+              //  'pagesplit': true,
+               'elementHandlers': specialElementHandlers
+             });
+             //doc.save('Test.pdf');
              // doc.setFont("calibri");
              doc.output('dataurlnewwindow');
            });
+
            $('#download').click(function(){
+             var doc = new jsPDF();
+             // We'll make our own renderer to skip this editor
+             var specialElementHandlers = {
+               '#editor': function(element, renderer){
+                 return true;
+               }
+             };
+
+             doc.fromHTML($('#book').get(0), 15, 15, {
+               'width': 170,
+               // 'margin': 1,
+              //  'pagesplit': true,
+               'elementHandlers': specialElementHandlers
+             });
+             //doc.save('Test.pdf');
+             // doc.setFont("calibri");
              // doc.setFont("calibri");
              doc.save('test.pdf');
            });
+
           //  $( "input[type='checkbox']" ).prop( "checked", function( i, val ) {
           //    if (val == true){
           //      console.log("clicked!")
