6d46dd824bd6129e56250ba36495d7497aa7316f
Junjie Li <jj.muyang@gmail.com>
2016-05-29 14:21:52 -0500

    remove unnecessary code and add some comments

--- static/js/PDFCtrl.js
+++ static/js/PDFCtrl.js
@@ -1,99 +1,105 @@
-
 $( "#searchButton" ).click(function() {
+  // this is the main search functionnality
   $('#loader').css("display","block")
-   var value = $( "#input" ).val();
+  var value = $( "#input" ).val();
   console.log(value);
   $.get( "/search", { searchQuery: value } )
-    .done(function( data ) {
-      console.log(data);
-      $("#articles").empty();
-      $("#clear").empty();
-      // var test;
-      $.each(data.articles, function(i,item){
-        var e = $('<div class="list-group">'+
-          '<a href="#design" class="list-group-item" >'+
-            '<h4>' + item.title + '</h4></a>' +
-            '<div class="list-group-item">' + item.summary + '</div>' +
-        '</div>');
-        $('#articles').append(e);
-        // $("#clear").empty();
-        $(e).bind("click",function () {
-          var tracker = 0;
-           $("#clear").empty();
-          //  for (var k = 1; k < sec.length-1; k++)
-           var sec = item.sections;
-          //  console.log("content:" + sec);
-           for (var k = 1; k < sec.length-1; k++){
-              if(sec[k].header != "") {
-                   var ee = $('<ul class=list-group>'+
-                       '<li class="list-group-item"><input id="section-' + k + '" type="checkbox" name="checkbox" value="' + sec[k].header +  " " + sec[k].content + '">' +'<h4>'+ sec[k].header +'</h4>'+ '</li>' +
-                      //  '<h4 class="list-group-item">' + sec[k].header + '</h4>' +
-                      //  '<div class="list-group-item">' + sec[k].content+ '</div>' +
-                   '</ul>');
+  .done(function( data ) {
+    console.log(data);
+    $("#articles").empty();
+    $("#clear").empty();
 
-              };
-              $('#clear').append(ee);
-           };
-           $("[id^=section-]").click(function() {
-             var num = this.id.slice(8);
-             var $elem = $('.section-'+num);
-             var id = $(this).attr('id');
-             var value = $('#' + id + '');
-             tracker = tracker + 1;
-             $('#table').append(
-               '<li>' + sec[num].header + '</li>'
-              );
-             $('#bookContent').append(
-               '<div id=' + id + ' class="list-group-item">' +
-               '<h3>' + tracker + '. ' + sec[num].header + '</h3>' +
-               '</br>' +
-             '<div>' + sec[num].content + '</div>' +
-             '</div>');
-           });
+    // Here is to append the articles and their summary
+    $.each(data.articles, function(i,item){
+      var e = $('<div class="list-group">'+
+      '<a href="#design" class="list-group-item" >'+
+      '<h4>' + item.title + '</h4></a>' +
+      '<div class="list-group-item">' + item.summary + '</div>' +
+      '</div>');
+      $('#articles').append(e);
 
-           $('#open').click(function(){
-             $("#title").empty();
-             $('#title').append( value
-              );
-             var doc = new jsPDF();
-             // We'll make our own renderer to skip this editor
-             var specialElementHandlers = {
-               '#editor': function(element, renderer){
-                 return true;
-               }
-             };
+      // here is to append the sub sections
+      $(e).bind("click",function () {
+        var tracker = 0;
+        $("#clear").empty();
 
-             doc.fromHTML($('#book').get(0), 15, 15, {
-               'width': 170,
-               // 'margin': 1,
-               'pagesplit': true,
-               'elementHandlers': specialElementHandlers
-             });
+        var sec = item.sections;
 
-             doc.output('dataurlnewwindow');
-           });
+        for (var k = 1; k < sec.length-1; k++){
+          if(sec[k].header != "") {
+            var ee = $('<ul class=list-group>'+
+            '<li class="list-group-item"><input id="section-' + k
+            + '" type="checkbox" name="checkbox" value="'
+            + sec[k].header +  " " + sec[k].content + '">' +'<h4>'
+            + sec[k].header +'</h4>'+ '</li>' +
 
-           $('#download').click(function(){
-             var doc = new jsPDF();
-             // We'll make our own renderer to skip this editor
-             var specialElementHandlers = {
-               '#editor': function(element, renderer){
-                 return true;
-               }
-             };
+            '</ul>');
 
-             doc.fromHTML($('#book').get(0), 15, 15, {
-               'width': 170,
-               // 'margin': 1,
-               'pagesplit': true,
-               'elementHandlers': specialElementHandlers
-             });
-             doc.save('test.pdf');
-           });
+          };
+          $('#clear').append(ee);
+        };
+        $("[id^=section-]").click(function() {
+          var num = this.id.slice(8);
+          var $elem = $('.section-'+num);
+          var id = $(this).attr('id');
+          var value = $('#' + id + '');
+          tracker = tracker + 1;
+          $('#table').append(
+            '<li>' + sec[num].header + '</li>'
+          );
+          $('#bookContent').append(
+            '<div id=' + id + ' class="list-group-item">' +
+            '<h3>' + tracker + '. ' + sec[num].header + '</h3>' +
+            '</br>' +
+            '<div>' + sec[num].content + '</div>' +
+            '</div>');
+          });
 
-         });
+          //  this is the click function for open a pdf page
+          $('#open').click(function(){
+            $("#title").empty();
+            $('#title').append( value
+            );
+            var doc = new jsPDF();
+            // We'll make our own renderer to skip this editor
+            var specialElementHandlers = {
+              '#editor': function(element, renderer){
+                return true;
+              }
+            };
+
+            doc.fromHTML($('#book').get(0), 15, 15, {
+              'width': 170,
+
+              'pagesplit': true,
+              'elementHandlers': specialElementHandlers
+            });
+
+            doc.output('dataurlnewwindow');
+          });
+
+          //  this is the click function for download
+          $('#download').click(function(){
+            var doc = new jsPDF();
+            // We'll make our own renderer to skip this editor
+            var specialElementHandlers = {
+              '#editor': function(element, renderer){
+                return true;
+              }
+            };
+
+            doc.fromHTML($('#book').get(0), 15, 15, {
+              'width': 170,
+              // 'margin': 1,
+              'pagesplit': true,
+              'elementHandlers': specialElementHandlers
+            });
+            doc.save('test.pdf');
+          });
 
         });
 
+      });
+
     });
-});
+  });
